00e85449c26265f6de572014d86031be2c01e77e
Tsiaro Fitia <fitia.rajs.12@gmail.com>
2025-03-15 21:55:14 +0300

    création de loadingscreenanimation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
 import NavBar from "./ui/NavBar";
 import MobileMenu from "./ui/MobileMenu";
+import LoadingScreenAnimation from "./LoadingScreenAnimation";
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
-    <div className="text-3xl font-bold underline">
+    <>
+      {!isLoading && (
+        <LoadingScreenAnimation onComplete={() => setIsLoading(true)} />
+      )}
       <NavBar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
       <MobileMenu isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
-    </div>
+    </>
   );
 }
